ace18eb6a4a4dd5f3c83780cb86bafb9f0ae9b8a
RabiKhan68 <Rabibro063@gmail.com>
2025-08-08 19:46:27 +0500

    initial

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.getElementById('hamburger');
   const navLinks = document.getElementById('navLinks');
+  const links = navLinks.querySelectorAll('a');
 
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
     hamburger.classList.toggle('active');
+  });
+
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      navLinks.classList.remove('show');
+      hamburger.classList.remove('active');
+    });
   });
 });
